f83438a12c3330d3d413c8b48b1cff8d0d573819
yusay1498 <u.yusay1498@gmail.com>
2025-05-02 11:55:34 +0900

    add scss

---
+++ src/pages/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from '../assets/images/react.svg'
+import viteLogo from '/vite.svg'
+import '../assets/styles/main.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>Demo App</div>
+    </>
+  )
+}
+
+export default App
